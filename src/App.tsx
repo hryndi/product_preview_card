294333a7fd7b6dfd0a09ddd72d63d15cca8b31de
Dmytro Hrynov <hrynovdmytro11@gmail.com>
2024-04-14 15:33:50 +0200

    finished product preview card.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import deskProductImg from "./assets/image-product-desktop.jpg";
+import mobileProductImg from "./assets/image-product-mobile.jpg";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <main>
+      <article className="product">
+        <picture className="product-image">
+          <source srcSet={deskProductImg} media="(min-width:600px)" />
+          <img src={mobileProductImg} alt="" />
+        </picture>
+
+        <div className="product-content">
+          <p className="category">Perfume</p>
+          <h1 className="header">Gabrielle Essence Eau De Parfum</h1>
+          <p className="description">
+            A floral, solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of
+            CHANEL.
+          </p>
+          <div className="flex-group">
+            <p className="price">149.99</p>
+            <p className="actual-price">
+              <s>169.99</s>
+            </p>
+          </div>
+          <button className="button" data-icon="shopping-cart">
+            Add to Cart
+          </button>
+        </div>
+      </article>
+    </main>
+  );
 }
 
 export default App;
